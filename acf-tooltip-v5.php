895d1b1a8b36ab2b7517ac584c36f0be7a3b656a
Thomas <t.meyer@me.com>
2014-12-23 11:00:59 +0100

    Initial commit

---
+++ acf-tooltip-v5.php
@@ -0,0 +1,9 @@
+<?php
+
+function my_enqueue() {
+    wp_enqueue_script( 'acf_tooltip_script', plugin_dir_url( __FILE__ ) . '/js/acf-tooltip-v5.js', array('jquery-qtip') );
+    wp_enqueue_style( 'acf_tooltip_css', plugin_dir_url( __FILE__ ) . '/css/acf-tooltip.css' );
+}
+add_action( 'acf/input/admin_head', 'my_enqueue' );
+
+?>
