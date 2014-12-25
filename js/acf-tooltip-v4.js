cf674fd8f6ca4991e4ffbbc504f0c13bde9fedea
Thomas <t.meyer@me.com>
2014-12-25 11:13:24 +0100

    Update for Repeater & Flexible Content Fields

--- js/acf-tooltip-v4.js
+++ js/acf-tooltip-v4.js
@@ -10,10 +10,21 @@
 			tooltiptext = $(this).html();
 			$(this).html('')
 			label.appendTo( $(this) );
-			if( !$.trim(tooltiptext) =='') {
-				label.append('<span class="tooltip" title="'+tooltiptext+'"> <span class="dashicons dashicons-editor-help"></span></span>');
-			}
-			
+			acf_label_tooltips(label, tooltiptext);
+		});
+
+		$('.repeater tr.row td.label, .acf-flexible-content .values.ui-sortable .layout tr.sub_field td.label').each(function() {
+			label = $(this).find('label');
+			tooltiptext = label.parent().find('.sub-field-instructions').html();
+			label.parent().find('.sub-field-instructions').remove();
+			acf_label_tooltips(label, tooltiptext);
+		});
+
+		$('.repeater th.acf-th-text, .acf-flexible-content .values.ui-sortable .layout th.acf-th-text').each(function() {
+			tooltiptext = $(this).find('.sub-field-instructions').html();
+			$(this).find('.sub-field-instructions').remove();
+			label = $(this).find('span');
+			acf_label_tooltips(label, tooltiptext);
 		});
 
 		$('.tooltip').qtip({
@@ -28,5 +39,14 @@
 		});
 
 	});
+
+	function acf_label_tooltips(labelfield, tooltiptext) {
+		if( !$.trim(tooltiptext) =='') {
+			if ( !label.hasClass('has_tooltip') ) {
+				label.append('<span class="tooltip" title="'+tooltiptext+'"> <span class="dashicons dashicons-editor-help"></span></span>');
+				label.addClass('has_tooltip');
+			}
+		}
+	};
 	
 })(jQuery);
